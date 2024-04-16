cd6b13475729bcdc0f9ef0b5b06bbdb0a5131523
DuleaM <mihaidulea@yahoo.com>
2024-04-16 20:02:23 +0300

    created the consumer class for sending requests to the server

---
+++ ProiectAA/ProiectAA/Consumer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ProiectAA
+{
+    public static class Consumer
+    {
+        private static TcpClient client = null;
+        private static NetworkStream stream = null;
+        private static StreamReader streamReader = null;
+        private static StreamWriter streamWriter = null;
+
+        public static void start_connection()
+        {
+            client = new TcpClient("localhost", 8000);
+            stream = client.GetStream();
+            streamReader = new StreamReader(stream);
+            streamWriter = new StreamWriter(stream);
+        }
+
+        public static void close_connection()
+        {
+            client.Close();
+            stream.Close();
+        }
+    }
+}
